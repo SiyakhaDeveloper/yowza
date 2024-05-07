4771ee3e10518c8d7e4235e6ca7dd00f693743d7
Mzwandile Dladla <mzwandiledladla7@gmail.com>
2024-05-07 11:41:34 +0200

    Replaced CKEditor with TinyMCE  for blog post content

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -74,9 +74,16 @@
                                     <label class="block">
                                 <textarea
                                     rows=""
-                                    placeholder=" Enter Text" id="editor" name="post_content"
-                                    class="form-textarea w-full rounded-lg border border-slate-300 bg-transparent p-2.5 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
-                                ></textarea>
+                                    placeholder=" Enter Text" name="post_content"
+                                    class="form-textarea
+                                    w-full rounded-lg border
+                                    border-slate-300 bg-transparent
+                                    p-2.5 placeholder:text-slate-400/70
+                                    hover:border-slate-400 focus:border-primary
+                                    dark:border-navy-450 dark:hover:border-navy-400
+                                    dark:focus:border-accent tinyMce">
+
+                                </textarea>
                                     </label>
                                     <br/>
                                     <label
